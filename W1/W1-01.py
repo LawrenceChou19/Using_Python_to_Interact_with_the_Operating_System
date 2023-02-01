bdf42d49e33d998cbd7c58c8472f69207499543c
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-02-02 01:53:16 +0800

    2023-02-02-Practice

--- W1/W1-01.py
+++ W1/W1-01.py
@@ -1,1 +1,8 @@
-#test
+import requests
+response = requests.get("http://www.google.com")
+print(len(response.text))
+
+import arrow
+date = arrow.get('2020-01-18','YYYY-MM-DD')
+date.shift(weeks=+6).format('MMM DD YYYY')
+print(date.shift(weeks=+6).format('MMM DD YYYY'))
