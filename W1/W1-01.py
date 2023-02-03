4f682fb41d17f368ae69536858b997f8e687ceaf
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-02-03 19:58:10 +0800

    2023-02-03

--- W1/W1-01.py
+++ W1/W1-01.py
@@ -6,3 +6,11 @@ import arrow
 date = arrow.get('2020-01-18','YYYY-MM-DD')
 date.shift(weeks=+6).format('MMM DD YYYY')
 print(date.shift(weeks=+6).format('MMM DD YYYY'))
+
+
+import pandas
+visitors = [1235,6424,9345,8464,2345]
+errors = [23,45,33,45,76]
+df = pandas.DataFrame({"visitors":visitors,"error":errors}, index=["Mon","Tues","Wed","Thu","Fri"])
+print(df)
+print(df["error"].mean())
