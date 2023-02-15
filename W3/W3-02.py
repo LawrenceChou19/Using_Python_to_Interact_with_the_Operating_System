d89b2a44237b45c81cf89262d5711c69bbbb8aa1
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-02-15 18:08:21 +0800

    2023-02-15

---
+++ W3/W3-02.py
@@ -0,0 +1,21 @@
+import re
+result = re.search(r"aza","plaza")
+print(result)
+
+result = re.search(r"aza","bazaar")
+print(result)
+
+result = re.search(r"aza","maze")
+print(result)
+
+result = re.search(r"p.ng","penguin")
+print(result)
+
+import re
+def check_aei (text):
+  result = re.search(r"a.e.i", text)
+  return result != None
+
+print(check_aei("academia")) # True
+print(check_aei("aerial")) # False
+print(check_aei("paramedic")) # True
