8d46a214794feb365728de4dc61430255910097f
LawrenceChou19 <LawrenceChou19@gmail.com>
2023-02-24 20:35:03 +0800

    2023-02-24

--- W4/W4-03.py
+++ W4/W4-03.py
@@ -14,9 +14,39 @@
 
 import re
 def show_time_of_pid(line):
-  pattern = r"(Jul\s\d\s\d*:\d*:\d*)"
-#   pattern = r"\d{5}"
+
+  pattern = r"(\w+ \d \d+\:\d+\:\d+) \w.+ [\w=_]+\[([0-9]+)\]"
+  #(\w+ \d \d+\:\d+\:\d+)= Jul 6 14:01:23, 
+  # \w.+ = computer.name  
+  # [\w=_]+\[([0-9]+)\] = CRON[29440] or jam_tag=psim[29187]
   result = re.search(pattern, line)
+#   print(result.groups())
+  return '{} pid:{}'.format(result[1],result[2])
+
+print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
+
+print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
+
+print(show_time_of_pid("Jul 6 14:02:09 computer.name jam_tag=psim[29187]: (UUID:007)")) # Jul 6 14:02:09 pid:29187
+
+print(show_time_of_pid("Jul 6 14:03:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:03:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:03:40 computer.name cacheclient[29807]: start syncing from \"0xDEADBEEF\"")) # Jul 6 14:03:40 pid:29807
+
+print(show_time_of_pid("Jul 6 14:04:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:04:01 pid:29440
+
+print(show_time_of_pid("Jul 6 14:05:01 computer.name CRON[29440]: USER (naughty_user)")) # Jul 6 14:05:01 pid:29440
+
+import re
+def show_time_of_pid(line):
+
+  pattern = r"(\w+ \d \d+\:\d+\:\d+) \w.+ [\w=_]+\[([0-9]+)\]: \w*"
+  #(\w+ \d \d+\:\d+\:\d+)= Jul 6 14:01:23, 
+  # \w.+ = computer.name  
+  # [\w=_]+\[([0-9]+)\] = CRON[29440] or jam_tag=psim[29187]
+  result = re.search(pattern, line)
+#   print(result.groups())
+#   return '{} pid:{}'.format(result[1],result[2])
   return result
 
 print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
